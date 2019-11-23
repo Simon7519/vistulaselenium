a740c80cb8093d39ef356d326ce9584cdc134efc
Simon7519 <46829505+Simon7519@users.noreply.github.com>
2019-11-23 15:12:54 +0100

    Praca domowa - fix4

--- src/test/java/org/vistula/selenium/test/part1/pracadomowa/ArenaTest.java
+++ src/test/java/org/vistula/selenium/test/part1/pracadomowa/ArenaTest.java
@@ -8,6 +8,9 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class ArenaTest {
 
@@ -15,6 +18,7 @@ public class ArenaTest {
 
     @BeforeClass
     public static void setDriver() {
+
         WebDriverManager.chromedriver().setup();
     }
 
@@ -26,37 +30,37 @@ public class ArenaTest {
         driver.manage().window().maximize();
         driver.get("http://demo.testarena.pl/zaloguj");
     }
+    @After
+    public void quit () {
+
+        driver.quit();
+
+    }
 
     @Test
-    public void loginTest() throws InterruptedException {
+    public void loginTest() {
 
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.SECONDS);
 
         driver.findElement(By.name("email")).sendKeys("administrator@testarena.pl");
         driver.findElement(By.name("password")).sendKeys("sumXQQ72$L");
         driver.findElement(By.id("save")).click();
-        Thread.sleep(5000);
+
         driver.findElement(By.className("icon_tools")).click();
-        Thread.sleep(5000);
+
         driver.findElement(By.className("button_link")).click();
-        Thread.sleep(5000);
+
         driver.findElement(By.name("name")).sendKeys("nowy projekt1");
         driver.findElement(By.name("prefix")).sendKeys("100");
         driver.findElement(By.id("save")).click();
-        Thread.sleep(5000);
+
         driver.findElement(By.className("activeMenu")).click();
-        Thread.sleep(5000);
+
         driver.findElement(By.id("search")).sendKeys("nowy projekt1");
         driver.findElement(By.id("j_searchButton")).click();
 
 
     }
 
-    @After
-    public void quit () {
-
-        driver.quit();
-
-
-    }
 
 }
