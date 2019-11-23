8b2d57a482a6bea40fa5ce7b89548ffd9847ce20
Simon7519 <46829505+Simon7519@users.noreply.github.com>
2019-11-23 14:14:41 +0100

    fix

--- src/test/java/org/vistula/selenium/test/testarena/TestArena.java
+++ src/test/java/org/vistula/selenium/test/testarena/TestArena.java
@@ -8,11 +8,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import static java.lang.Thread.sleep;
-import static org.openqa.selenium.Keys.ENTER;
 import org.junit.Test;
 
 
